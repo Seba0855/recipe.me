4e3f0e200b538351fb6ed328a81b2b4f29b01f99
Seba0855 <sebastian08555@gmail.com>
2023-03-28 00:39:50 +0200

    finished versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,10 @@ import pl.smcebi.recipeme.buildSrc.Libs
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.dagger.hilt) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
 
 buildscript {
@@ -15,14 +16,13 @@ buildscript {
     }
     dependencies {
         classpath(libs.gradle.plugin)
-//        classpath(Libs.Kotlin.gradlePlugin)
-//        classpath(Libs.Android.gradlePlugin)
-//        classpath(Libs.Google.servicesGradlePlugin)
-//        classpath(Libs.Firebase.crashlyticsGradlePlugin)
-//        classpath(Libs.Firebase.performanceGradlePlugin)
-//        classpath(Libs.Navigation.safeArgsGradlePlugin)
-//        classpath(Libs.Dagger.hiltGradlePlugin)
-//        classpath(Libs.Kotlin.Serialization.gradlePlugin)
+        classpath(libs.kotlin.gradlePlugin)
+        classpath(libs.google.servicesGradlePlugin)
+        classpath(libs.firebase.crashlyticsGradlePlugin)
+        classpath(libs.firebase.performanceGradlePlugin)
+        classpath(libs.navigation.safeargs.plugin)
+        classpath(libs.dagger.hiltGradlePlugin)
+        classpath(libs.kotlin.serialization.gradlePlugin)
     }
 }
 
