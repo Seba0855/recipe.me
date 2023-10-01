55d6561aafa54b6f3f3e62cfbbf42b95d7d94d51
Seba0855 <matuszczyksebastian9@gmail.com>
2023-10-01 21:53:38 +0200

    UI Scanner module with dependencies

---
+++ ui-scanner/build.gradle.kts
@@ -0,0 +1,62 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.navigation.safeargs)
+    alias(libs.plugins.kotlin.parcelize)
+    id("pl.smcebi.recipeme.library")
+}
+
+android {
+    namespace = "pl.smcebi.recipeme.ui.scanner"
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":domain-common"))
+    implementation(project(":domain-recipes"))
+    implementation(project(":ui-common"))
+
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(libs.androidx.recyclerview)
+
+    // UI
+    implementation(libs.google.material)
+    implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+
+    implementation(libs.glide)
+    implementation(libs.lottie)
+
+    // Camera & Analysis
+    implementation(libs.androidx.camerax.core)
+    implementation(libs.androidx.camerax.lifecycle)
+    implementation(libs.androidx.camerax.view)
+    implementation(libs.google.zxing)
+
+    // Dagger
+    implementation(libs.dagger.hiltLib)
+    kapt(libs.dagger.hiltProc)
+    implementation(libs.dagger.daggerLib)
+    kapt(libs.dagger.daggerProc)
+
+    // Utils
+    implementation(libs.timber)
+
+    testImplementation(libs.junit.core)
+    androidTestImplementation(libs.junit.testExt)
+    androidTestImplementation(libs.espresso.core)
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
---
+++ ui-scanner/src/main/java/pl/smcebi/recipeme/ui/scanner/ScannerFragment.kt
@@ -0,0 +1,8 @@
+package pl.smcebi.recipeme.ui.scanner
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+internal class ScannerFragment : Fragment(R.layout.fragment_scanner) {
+}
