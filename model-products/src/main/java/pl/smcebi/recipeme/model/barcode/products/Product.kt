d31984a940f189f176ab059085b6644fc511f30e
Seba0855 <sebastian08555@gmail.com>
2023-10-29 23:55:44 +0100

    Fixed product serialization

--- model-products/src/main/java/pl/smcebi/recipeme/model/barcode/products/Product.kt
+++ model-products/src/main/java/pl/smcebi/recipeme/model/barcode/products/Product.kt
@@ -5,10 +5,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Product(
-    @SerialName("_keywords") val keywords: List<String>,
-    @SerialName("product_name") val productName: String,
-    @SerialName("selected_images") val selectedImages: ProductImage,
-    val brands: String,
-    val categories: String,
-    val quantity: String,
+    @SerialName("_keywords") val keywords: List<String> = emptyList(),
+    @SerialName("product_name") val productName: String = "",
+    @SerialName("selected_images") val selectedImages: ProductImage? = null,
+    val brands: String = "",
+    val categories: String = "",
+    val quantity: String = "",
 )
