d007c117ae77640fd42a4766ad2574cd12b67612
Seba0855 <sebastian08555@gmail.com>
2023-12-29 00:54:49 +0100

    Product list fragment layout

--- ui-scanner/src/main/java/pl/smcebi/recipeme/ui/scanner/products/ProductListFragment.kt
+++ ui-scanner/src/main/java/pl/smcebi/recipeme/ui/scanner/products/ProductListFragment.kt
@@ -2,7 +2,10 @@ package pl.smcebi.recipeme.ui.scanner.products
 
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import pl.smcebi.recipeme.ui.scanner.R
 
 @AndroidEntryPoint
-internal class ProductListFragment : Fragment() {
+internal class ProductListFragment : Fragment(R.layout.fragment_product_list) {
+
+
 }
