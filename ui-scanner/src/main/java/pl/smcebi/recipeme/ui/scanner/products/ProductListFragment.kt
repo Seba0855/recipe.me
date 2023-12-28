d246752543dba37385df6cc1bf9eeac62fd83978
Seba0855 <sebastian08555@gmail.com>
2023-12-28 22:09:43 +0100

    Scannet product card

---
+++ ui-scanner/src/main/java/pl/smcebi/recipeme/ui/scanner/products/ProductListFragment.kt
@@ -0,0 +1,8 @@
+package pl.smcebi.recipeme.ui.scanner.products
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+internal class ProductListFragment : Fragment() {
+}
